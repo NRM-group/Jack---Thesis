9d59722ded75f44894602a64b5da0fc041f5e7e3
jackmcnamarauq <101162678+jackmcnamarauq@users.noreply.github.com>
2022-06-13 10:00:05 +1000

    Update PointCloudVisualisation.py

--- mrn_movo/src/mrn_movo/PointCloudVisualisation.py
+++ mrn_movo/src/mrn_movo/PointCloudVisualisation.py
@@ -105,30 +105,12 @@ if __name__ == "__main__":
     xyz = np.vstack((X.flatten(), Y.flatten(), Z.flatten())).T
     
     
-    # X_filt[:, 2] = -1 * X_filt[:, 2]
-    
-    # fig1 = plt.figure(1)
-    # pcl_to_3d(X_filt)
-    
-    # # fig2 = plt.figure(2)
-    # # pcl_to_pca(X_filt)
-    
-    # hullToSurface(ConvexHull(X_filt)).plot()
-    
-    # visualise_pcl(X_raw_chest_wrist_right)
     visualise_pcl(X_filt)
 
     
     pcd = o3d.geometry.PointCloud()
     pcd.points = o3d.utility.Vector3dVector(X_filt)
-    # pcd_cube = o3d.geometry.PointCloud()
-    # pcd_cube.points = o3d.utility.Vector3dVector(xyz)
-    
-    # mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
-    # o3d.visualization.draw_geometries([pcd, pcd_cube, mesh_frame])
-    
-    # pcd.estimate_normals(
-    # search_param=o3d.geometry.KDTreeSearchParamHybrid(radius=0.1, max_nn=30))
+
     
     
     alpha = 0.3
@@ -139,77 +121,11 @@ if __name__ == "__main__":
     mesh.compute_vertex_normals()
     mesh_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(size=0.1, origin=[0, 0, 0])
     o3d.visualization.draw_geometries([mesh, mesh_frame], mesh_show_back_face=True)
-    # volume = o3d.geometry.TriangleMesh.get_volume(mesh)
-    # print(volume)
-    o3d.io.write_triangle_mesh("miguel_3.ply", mesh)
-    surface_area = o3d.geometry.TriangleMesh.get_surface_area(mesh)
+    volume = o3d.geometry.TriangleMesh.get_volume(mesh)
+    print(volume)
+
     
-    fig = plt.figure()
-    ax =  fig.add_subplot(111)
-    data_jack = np.array([0.03578766801815441, 0.058, 0.0745773768502948, 0.040326225014803655, 0.04257842653133096])
-    print(np.mean(data_jack))
-    data_jack_diff = [data_jack[0]-data_jack[1],
-                      data_jack[0]-data_jack[2],
-                      data_jack[0]-data_jack[3],
-                      data_jack[0]-data_jack[4],
-                      data_jack[1]-data_jack[2],
-                      data_jack[1]-data_jack[3],
-                      data_jack[1]-data_jack[4],
-                      data_jack[2]-data_jack[3],
-                      data_jack[2]-data_jack[4],
-                      data_jack[3]-data_jack[4]]
-    abs_jack =  [abs(ele) for ele in data_jack_diff]
-    print(np.mean(abs_jack))
-    print(np.median(abs_jack))
-    q75, q25 = np.percentile(abs_jack, [75, 25])
-    print("q75: %s"%q75)
-    print("q25: %s"%q25)
-    # matplotlib.pyplot.boxplot(abs_jack, notch=None, vert=None, patch_artist=None, widths=None)
-    
-    data_miguel = np.array([0.08861533777599271, 0.07272004578265817, 0.05584684596283472, 0.052479996857135802, 0.061460487930597962])
-    print(np.mean(data_miguel))
-    data_miguel_diff = [data_miguel[0]-data_miguel[1],
-                      data_miguel[0]-data_miguel[2],
-                      data_miguel[0]-data_miguel[3],
-                      data_miguel[0]-data_miguel[4],
-                      data_miguel[1]-data_miguel[2],
-                      data_miguel[1]-data_miguel[3],
-                      data_miguel[1]-data_miguel[4],
-                      data_miguel[2]-data_miguel[3],
-                      data_miguel[2]-data_miguel[4],
-                      data_miguel[3]-data_miguel[4]]
-    abs_miguel =  [abs(ele) for ele in data_miguel_diff]
-    print(np.mean(abs_miguel))
-    print(np.median(abs_miguel))
-    q75, q25 = np.percentile(abs_miguel, [75, 25])
-    print("q75: %s"%q75)
-    print("q25: %s"%q25)
-    data_jack_2 = np.array([0.02578766801815441, 0.0376850294818, 0.0685029487457737, 0.048036554032622501, 0.06531330962257842])
-    print(np.mean(data_jack_2))
- 
-    data_jack_2_diff = [data_jack_2[0]-data_jack_2[1],
-                      data_jack_2[0]-data_jack_2[2],
-                      data_jack_2[0]-data_jack_2[3],
-                      data_jack_2[0]-data_jack_2[4],
-                      data_jack_2[1]-data_jack_2[2],
-                      data_jack_2[1]-data_jack_2[3],
-                      data_jack_2[1]-data_jack_2[4],
-                      data_jack_2[2]-data_jack_2[3],
-                      data_jack_2[2]-data_jack_2[4],
-                      data_jack_2[3]-data_jack_2[4]]
-    abs_jack_2 =  [abs(ele) for ele in data_jack_2_diff]
-    print(np.mean(abs_jack_2))
-    print(np.median(abs_jack_2))
-    q75, q25 = np.percentile(abs_jack_2, [75, 25])
-    print("q75: %s"%q75)
-    print("q25: %s"%q25)    
-    data = [abs_jack, abs_miguel, abs_jack_2]
-    
-    matplotlib.pyplot.boxplot(data, notch=None, vert=None, patch_artist=None, widths=None, showmeans=True)
-    ax.set_title('Difference in Reconstructed Workspace Volume')
-    ax.set_xticklabels(["h1", 'h2', 'h3'])
-    ax.set_ylabel('Difference in Volume ($m^3$)')
-    matplotlib.pyplot.show()
+  
     
     
 
